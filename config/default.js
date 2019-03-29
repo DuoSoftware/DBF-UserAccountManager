d803df5859060fe411e4473521d8452701fac147
Binara Goonawardana <binara.is@live.com>
2019-03-29 12:24:31 +0530

    added default.js

---
+++ config/default.js
@@ -0,0 +1,18 @@
+module.exports = {
+  Host: {
+    port: 3030,
+    version: "1.0.0"
+  },
+  Cognito: {
+    Region:  "",
+    UserPoolId: "",
+  },
+  Mongo: {
+    ip: "",
+    port: "",
+    dbname: "",
+    password: "",
+    user: "",
+    cloudAtlas: true
+  },
+}
